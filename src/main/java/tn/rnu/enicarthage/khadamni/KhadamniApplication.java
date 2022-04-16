dc1f12cdd525ef12d26c432ced4d1a8b7e15e2a1
HtewechIheb <ihebhtewech@gmail.com>
2022-04-16 01:07:21 +0100

    Initial setup

---
+++ src/main/java/tn/rnu/enicarthage/khadamni/KhadamniApplication.java
@@ -0,0 +1,13 @@
+package tn.rnu.enicarthage.khadamni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhadamniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhadamniApplication.class, args);
+	}
+
+}

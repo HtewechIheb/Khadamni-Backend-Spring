0737f8a2946acace632a3d2f0fd689b4cf47ba63
HtewechIheb <ihebhtewech@gmail.com>
2022-04-27 17:09:55 +0100

    Excluded company photo route from authentication

--- src/main/java/tn/rnu/enicarthage/khadamni/configuration/SecurityConfig.java
+++ src/main/java/tn/rnu/enicarthage/khadamni/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package tn.rnu.enicarthage.khadamni.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api-docs/**").permitAll()
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/companies/{id}/photo").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
